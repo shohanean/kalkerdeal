d77cd12aafbef3b9fdc36d91dc49d2112b01ffbe
Rayan Hossain <hossain_shohan1@live.com>
2023-07-19 16:24:13 +0600

    check

--- routes/api.php
+++ routes/api.php
@@ -22,6 +22,6 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
 Route::group(['prefix' => 'v1'], function () {
     Route::post('/auth/register', [AuthController::class, 'register']);
     Route::post('/auth/login', [AuthController::class, 'login']);
-    Route::get('/categories', [CategoryController::class, 'index']);
-    Route::get('/categories/{id}', [CategoryController::class, 'show']);
+    // Route::get('/categories', [CategoryController::class, 'index']);
+    // Route::get('/categories/{id}', [CategoryController::class, 'show']);
 });
